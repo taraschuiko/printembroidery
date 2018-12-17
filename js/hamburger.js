7ad6a6a11cff6415043361bc5bab2cea57a8a767
taraschuiko <taraschuiko@gmail.com>
2018-12-17 17:02:23 +0200

    Improved responsiveness on index.html

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function ($) {
-  $(".hamburger-icon, .hamburger-fade").on("click", function (t) {
+  $(".hamburger-icon svg, .hamburger-fade").on("click", function (t) {
     t.preventDefault();
     $(".hamburger").toggleClass("hamburger--closed");
     $(".hamburger-fade").toggleClass("hamburger-fade--closed");
