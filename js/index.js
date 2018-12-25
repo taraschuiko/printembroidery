13f92ac8904b3231bcf991e8b7340cc9e52c1d7e
taraschuiko <taraschuiko@gmail.com>
2018-12-25 13:14:12 +0200

    Made home page video load only on medium devices and larger

---
+++ js/index.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function ($) {
+  var video = document.querySelectorAll(".header-video")[0];
+  var source = document.querySelectorAll(".header-video source")[0];
+
+  if ($(window).width() > 768) {
+    var src = source.getAttribute("data-src");
+    source.setAttribute("src", src);
+    video.load();
+  }
+});
