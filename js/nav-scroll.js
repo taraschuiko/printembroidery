0643db2022d11c20e238819a55381267bddccb03
taraschuiko <taraschuiko@gmail.com>
2018-12-27 15:55:54 +0200

    Made direct printing page and made few fixes on home page

--- js/nav-scroll.js
+++ js/nav-scroll.js
@@ -1,10 +1,13 @@
+var $prevImage = $('#nav .nav-logo img').attr("src");
+
 $(window).scroll(function () {
   var $height = $(window).scrollTop();
+
   if ($height > 40) {
     $('#nav').addClass('nav-sticky');
-    $('#nav .nav-logo img').attr("src", "./data/img/logo-dark.svg");
+    $('#nav .nav-logo img').attr("src", "/data/img/logo-dark.svg");
   } else {
     $('#nav').removeClass('nav-sticky');
-    $('#nav .nav-logo img').attr("src", "./data/img/logo-light.svg");
+    $('#nav .nav-logo img').attr("src", $prevImage);
   }
 });
