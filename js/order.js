d9abff203e67353b932a0d38d9bfb7f2cf2dd2e6
taraschuiko <taraschuiko@gmail.com>
2019-01-06 15:08:08 +0200

    Made order form without working close button

--- js/order.js
+++ js/order.js
@@ -1,10 +1,23 @@
 jQuery(document).ready(function ($) {
+  var order = $("#order");
+  var fade = $(".fade");
+
   $(".js-btn-order").click(function (event) {
     event.preventDefault();
-    $("#order").load("/reusable/order.html");
-    $("#order").addClass("order");
-    $(".fade").addClass("show");
-  })
+    order.load("/reusable/order.html");
+    order.addClass("order");
+    fade.addClass("show");
+  }, addCloseButtonListener)
+
+  var addCloseButtonListener = function () {
+    console.log($(".js-btn-order-close"));
+
+    $(".js-btn-order-close").click(function (event) {
+      order.removeClass("order");
+      fade.removeClass("show");
+      console.log("works");
+    })
+  }
 
   // $("#order").load("/reusable/order.html");
   // $("#order").addClass("order");
