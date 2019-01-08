b78d83aabb70e912376a7d1ccff0977297a119b2
taraschuiko <taraschuiko@gmail.com>
2019-01-08 12:16:19 +0200

    Fixed order window width and closing

--- js/order.js
+++ js/order.js
@@ -2,24 +2,27 @@ jQuery(document).ready(function ($) {
   var order = $("#order");
   var fade = $(".fade");
 
-  $(".js-btn-order").click(function (event) {
+  // Open order window
+  $(".js-order-open").on('click', openOrder);
+
+  function openOrder() {
     event.preventDefault();
     order.load("/reusable/order.html");
     order.addClass("order");
     fade.addClass("show");
-  }, addCloseButtonListener)
-
-  var addCloseButtonListener = function () {
-    console.log($(".js-btn-order-close"));
-
-    $(".js-btn-order-close").click(function (event) {
-      order.removeClass("order");
-      fade.removeClass("show");
-      console.log("works");
-    })
   }
 
-  // $("#order").load("/reusable/order.html");
-  // $("#order").addClass("order");
-  // $(".fade").addClass("show");
+  // Close order window
+  order.on('click', '.js-order-close', closeOrder);
+  fade.on('click', closeOrder);
+  order.on('click', function (e) {
+    if (e.target == this)
+      closeOrder();
+  });
+
+  function closeOrder() {
+    order.removeClass("order");
+    order.html("");
+    fade.removeClass("show");
+  }
 })
--- order.php
+++ order.php
@@ -1,25 +1,28 @@
 <?php
-  // Get values
-  $services = '';
-  if(!empty($_POST['services'])) {    
-      foreach($_POST['services'] as $value) {
-        if($services == '') {
-          $services = $value;
-        } else {
-          $services = $services.', '.$value;
+
+  if(isset($_POST['submit'])) {
+    // Get values
+    $services = '';
+    if(!empty($_POST['services'])) {    
+        foreach($_POST['services'] as $value) {
+          if($services == '') {
+            $services = $value;
+          } else {
+            $services .= ', '.$value;
+          }
         }
-      }
+    }
+    $name = $_POST['name'];
+    $tel = $_POST['tel'];
+    $comment = $_POST['comment'];
+
+    // Send email
+    $message = '<h1>Нове замовлення від '.$name."!</h1><br>"
+      .'Номер телефону: '.$tel."<br>"
+      .'Коментар: '.$comment."<br>"
+      .'Послуги: '.$services;
+
+    echo $message;
+    mail("info@printembroidery.com.ua", "Нове замовлення!", $message, "Content-Type: text/html; charset=UTF-8");
   }
-  $name = $_POST['name'];
-  $tel = $_POST['tel'];
-  $comment = $_POST['comment'];
-
-  // Send email
-  $message = '<h1>Нове замовлення від '.$name."!</h1><br>"
-    .'Номер телефону: '.$tel."<br>"
-    .'Коментар: '.$comment."<br>"
-    .'Послуги: '.$services;
-
-  echo $message;
-  mail("info@printembroidery.com.ua", "Нове замовлення!", $message, "Content-Type: text/html; charset=UTF-8");
 ?>
