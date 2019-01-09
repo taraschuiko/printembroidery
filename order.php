3135ec781c77fef93d9bb2495b42ed3cad0a2d7d
taraschuiko <taraschuiko@gmail.com>
2019-01-09 15:33:28 +0200

    Made file uploading work

--- order.php
+++ order.php
@@ -1,7 +1,12 @@
 <?php
+  $currentDir = getcwd();
+  $uploadDirectory = "/uploads/";
 
-  if(isset($_POST['submit'])) {
-    // Get values
+  $errors = []; // Store all foreseen and unforseen errors here
+
+  $fileExtensions = ['jpeg','jpg','png','gif','psd','ai','cdr']; // Get all the file extensions
+
+  // Get values
     $services = '';
     if(!empty($_POST['services'])) {    
         foreach($_POST['services'] as $value) {
@@ -16,13 +21,44 @@
     $tel = $_POST['tel'];
     $comment = $_POST['comment'];
 
-    // Send email
-    $message = '<h1>Нове замовлення від '.$name."!</h1><br>"
-      .'Номер телефону: '.$tel."<br>"
-      .'Коментар: '.$comment."<br>"
-      .'Послуги: '.$services;
+  $fileName = $_FILES['file']['name'];
+  $fileSize = $_FILES['file']['size'];
+  $fileTmpName  = $_FILES['file']['tmp_name'];
+  $fileType = $_FILES['file']['type'];
+  $fileExtension = strtolower(end(explode('.',$fileName)));
 
-    echo $message;
-    mail("info@printembroidery.com.ua", "Нове замовлення!", $message, "Content-Type: text/html; charset=UTF-8");
+  $uploadPath = $currentDir . $uploadDirectory . basename($fileName); 
+
+  if(isset($_POST['submit'])) {
+    // File check
+    if (! in_array($fileExtension,$fileExtensions)) {
+            $errors[] = "Файл повинен бути у форматі .png, .jpg, .gif, .psd, .ai або .cdr";
+        }
+
+        if ($fileSize > 5000000) {
+            $errors[] = "Розмір Вашого файлу більший, ніж 5МБ. Завантажте, будь ласка, менший файл.";
+        }
+
+        if (empty($errors)) {
+            $didUpload = move_uploaded_file($fileTmpName, $uploadPath);
+
+            if ($didUpload) {
+                // Send email
+                $message = '<h1>Нове замовлення від '.$name."!</h1><br>"
+                .'Номер телефону: '.$tel."<br>"
+                .'Коментар: '.$comment."<br>"
+                .'Послуги: '.$services."<br>"
+                .'Файл: <a href="http://new.printembroidery.com.ua/uploads/'.$fileName.'">'.$fileName.'</a>';
+
+                mail("info@printembroidery.com.ua", "Нове замовлення!", $message, "Content-Type: text/html; charset=UTF-8");
+                header("Location:/order-success/");
+            } else {
+                echo "Десь помилка, напиши нам про це.";
+            }
+        } else {
+            foreach ($errors as $error) {
+                echo $error . " Це помилки" . "\n";
+            }
+        }
   }
 ?>
