281cd5ff91aa73af39231ef19eba21e214d44be8
taraschuiko <taraschuiko@gmail.com>
2019-01-12 13:11:24 +0200

    Fixed scroll bars in order window in different browsers

--- js/order.js
+++ js/order.js
@@ -3,14 +3,16 @@ jQuery(document).ready(function ($) {
   var fade = $(".fade");
 
   // Open order window
-  $(".js-order-open").on('click', openOrder);
-
-  function openOrder() {
+  $(".js-order-open").click(function (event) {
     event.preventDefault();
-    order.load("/reusable/order.html");
+    order.load("/reusable/order.html", function () {
+      if (order.outerHeight() == $(window).height()) {
+        order.css("overflow", "scroll");
+      }
+    });
     order.addClass("order-modal");
     fade.addClass("show");
-  }
+  });
 
   // Close order window
   order.on('click', '.js-order-close', closeOrder);
