750f18e6c1428825089c3281c1b5d08cfc6e75b1
taraschuiko <taraschuiko@gmail.com>
2019-02-08 08:54:16 +0200

    Added /ru/ folder with ukrainian content, translated menu on home page

---
+++ ru/js/order.js
@@ -0,0 +1,44 @@
+jQuery(document).ready(function ($) {
+  var modal = $("#modal");
+  var fade = $(".fade");
+
+  // Open order window
+  $(".js-order-open").click(function (event) {
+    event.preventDefault();
+    modal.load("/reusable/order.html", function () {
+      if (modal.outerHeight() < $(window).height()) {
+        modal.css("overflow-y", "hidden");
+      }
+    });
+    modal.addClass("order-modal");
+    fade.addClass("show");
+  });
+
+  // Close order window
+  modal.on('click', '.js-order-close', closeOrder);
+  fade.on('click', closeOrder);
+  modal.on('click', function (e) {
+    if (e.target == this)
+      closeOrder();
+  });
+
+  function closeOrder() {
+    modal.removeClass("order-modal");
+    modal.html("");
+    fade.removeClass("show");
+  }
+
+  // Change button text on submit
+
+  $(document).on("submit", ".order-form", function (e) {
+    var button = $(e.target).find("input[type=submit]")
+    var val = button.val();
+    if (val === "Замовити") {
+      val = "Замовлення надсилається";
+    } else if (val === "Надіслати") {
+      val = "Повідомлення надсилається"
+    }
+    button.val(val);
+    button.attr("disabled", "disabled");
+  })
+})
---
+++ ru/message.php
@@ -0,0 +1,18 @@
+<?php
+  // Get values
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $text = $_POST['message'];
+
+    $message = '<h1>Нове повідомлення від '.$name."!</h1><br>"
+                .'Email: '.$email."<br>"
+                .'Повідомлення: '.$text;
+                
+      if(!(mail("info@printembroidery.com.ua", "Нове повідомлення!", $message, "Content-Type: text/html; charset=UTF-8"))) {
+        header("Location:/message/fail.html");
+        exit;
+      }
+      // Направити на сторінку успіху
+      header("Location:/message/success.html");
+      exit;
+?>
